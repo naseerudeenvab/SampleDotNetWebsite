d3b8305995c8388796b74fd2d54b06b6cd2490cd
Nvab <Naseerudeen.Vab@quantium.com.au>
2020-09-11 16:48:42 +1000

    Add project files.

---
+++ Controllers/ParticipantsController.cs
@@ -0,0 +1,141 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using SampleWebsite.Models;
+
+namespace SampleWebsite.Controllers
+{
+    public class ParticipantsController : Controller
+    {
+        private WebAppEntities db = new WebAppEntities();
+
+        // GET: Participants
+        public ActionResult Index()
+        {
+            return View(db.Participants.ToList());
+        }
+
+        // GET: Participants/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Participant participant = db.Participants.Find(id);
+            if (participant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(participant);
+        }
+
+        // GET: Participants/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Participants/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Department,IsHost")] Participant participant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Participants.Add(participant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(participant);
+        }
+
+        // GET: Participants/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Participant participant = db.Participants.Find(id);
+            if (participant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(participant);
+        }
+
+        // POST: Participants/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Department,IsHost")] Participant participant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(participant).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(participant);
+        }
+
+        // GET: Participants/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Participant participant = db.Participants.Find(id);
+            if (participant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(participant);
+        }
+
+        // GET: Participants/MakeAdmin/5
+        public ActionResult ChangeUser(int? id, bool isHost)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Participant participant = db.Participants.Find(id);
+            if (participant == null)
+            {
+                return HttpNotFound();
+            }
+
+            participant.IsHost = isHost;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // POST: Participants/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Participant participant = db.Participants.Find(id);
+            db.Participants.Remove(participant);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
